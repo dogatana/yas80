299278fd343b80e9111d7b5bb2c200186de73d40
Toshihiko Ichida <dogatana@gmail.com>
2025-10-26 20:51:04 +0900

    INST2 構文解析テスト追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -87,6 +87,39 @@ RST 17
 	}
 }
 
+func TestINST2(t *testing.T) {
+	input := `
+EX AF, AF'
+ADD A, 1
+ADC A, 2
+SBC A, 3
+BIT 0, B
+SET 1, C
+RES 2, D
+JP 100
+JP C, 100
+JR 200
+JR NC, 200
+CALL 300
+CALL NC, 300
+IN A, (1)
+IN B, (C)
+OUT (2), A
+OUT (C), B
+`
+	l := NewLexer(bufio.NewReader(strings.NewReader(input)))
+	ret := Parse(l)
+	if ret != 0 {
+		t.Errorf("Parse returns %d", ret)
+	}
+	expected := strings.Trim(input, " \n\t")
+	text := strings.ReplaceAll(Root.String(), "\t", " ")
+	if text != expected {
+		t.Errorf("program differs. exptected %d chars. got %d chars",
+			len(expected), len(text))
+	}
+}
+
 func TestIndirect(t *testing.T) {
 	input := `
 LD A, (HL)
