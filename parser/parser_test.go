853483fee4a25b5fe5edd4221d2e25e3a46a8515
Toshihiko Ichida <dogatana@gmail.com>
2025-10-26 12:04:31 +0900

    INST1 のテスト追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -47,6 +48,40 @@ OTDR
 	}
 	expected := strings.Trim(input, " \n\t")
 	text := Root.String()
+	if text != expected {
+		t.Errorf("program differs. exptected %d chars. got %d chars",
+			len(expected), len(text))
+	}
+}
+
+func TestINST1(t *testing.T) {
+	input := `
+PUSH 1
+POP 2
+SUB 3
+AND 4
+OR 5
+CP 6
+INC 7
+DEC 8
+IM 9
+RLC 10
+RL 11
+RR 12
+SLA 13
+SRA 14
+SRL 15
+DJNZ 16
+RST 17
+`
+	l := NewLexer(bufio.NewReader(strings.NewReader(input)))
+	ret := Parse(l)
+	if ret != 0 {
+		t.Errorf("Parse returns %d", ret)
+	}
+	expected := strings.Trim(input, " \n\t")
+	text := strings.ReplaceAll(Root.String(), "\t", " ")
+	fmt.Println(text)
 	if text != expected {
 		t.Errorf("program differs. exptected %d chars. got %d chars",
 			len(expected), len(text))
