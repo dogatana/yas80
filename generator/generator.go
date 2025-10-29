859660463c533c2b008828b16c7004b6a6d3a99a
Toshihiko Ichida <dogatana@gmail.com>
2025-10-29 21:26:04 +0900

    RET 命令生成

--- generator/generator.go
+++ generator/generator.go
@@ -21,15 +21,39 @@ func (g *Generator) Generate() {
 	for _, node := range g.program.Statements {
 		switch node.NodeType() {
 		case parser.Z80_INST0:
-			inst := node.(*parser.Z80Instruction)
-			info := Z80CodeTable0[int(inst.OpCode)]
-			obj := &object.FixedCode{Line: inst.LineNumber, Code: make([]byte, len(info.Bytes))}
+			node := node.(*parser.Z80Instruction)
+			info := Z80CodeTable0[int(node.OpCode)]
+			obj := &object.FixedCode{Line: node.LineNumber, Code: make([]byte, len(info.Bytes))}
 			copy(obj.Code, info.Bytes)
 			g.objects = append(g.objects, obj)
+		case parser.Z80_INST1:
+			node := node.(*parser.Z80Instruction)
+			if node.OpCode == parser.Z80_INST_RET {
+				g.generateRET(node)
+				break
+			}
+			fmt.Println(parser.NodeTypeNames(node.NodeType()))
 		default:
 			fmt.Println(parser.NodeTypeNames(node.NodeType()))
 		}
 	}
+}
+
+func (g *Generator) generateRET(node *parser.Z80Instruction) {
+	if node.Op1 == nil {
+		g.objects = append(g.objects,
+			&object.FixedCode{Line: node.LineNumber, Code: []byte{0xc9}})
+		return
+	}
+	if node.Op1.NodeType() == parser.Z80_FLAG {
+		flag := int(node.Op1.NodeSubType()) - parser.Z80_FLAG_NZ
+		b := byte(0xc0 | flag<<3)
+		g.objects = append(g.objects,
+			&object.FixedCode{Line: node.LineNumber, Code: []byte{b}})
+		return
+	}
+	g.es.AddError("", node.LineNumber,
+		fmt.Sprintf("第1オペランドがフラグではありません '%s'", node.Op1.String()))
 }
 
 func (g *Generator) Dump() {
