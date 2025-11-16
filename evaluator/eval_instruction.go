17d79e70dae244078a49116431a923b28a77f444
Toshihiko Ichida <dogatana@gmail.com>
2025-11-16 14:57:10 +0900

    evaluator.go から eval_instruction.go を分離

---
+++ evaluator/eval_instruction.go
@@ -0,0 +1,102 @@
+package evaluator
+
+import (
+	"fmt"
+	"yas80/logger"
+	"yas80/object"
+	"yas80/parser"
+)
+
+func (e *Evaluator) evalZ80Instruction(node *parser.Z80Instruction, env *object.Environment) object.Object {
+	switch node.NodeType() {
+	case parser.Z80_INST0:
+		info := Z80CodeTable0[int(node.OpCode)]
+		obj := &object.Code{Line: node.LineNumber(), Code: make([]byte, len(info.Bytes))}
+		copy(obj.Code, info.Bytes)
+		return obj
+	case parser.Z80_INST1:
+		if node.OpCode == parser.Z80_INST_RET {
+			return e.generateRET(node, env)
+		}
+		return object.NULL
+	case parser.Z80_INST2:
+		return e.evalZ80Instruction2(node, env)
+	default:
+		return object.NULL
+	}
+}
+
+func (e *Evaluator) generateRET(node *parser.Z80Instruction, env *object.Environment) object.Object {
+	if node.Op1 == nil {
+		return &object.Code{Line: node.LineNumber(), Code: []byte{0xc9}}
+	}
+	if node.Op1.NodeType() == parser.Z80_FLAG {
+		flag := int(node.Op1.NodeSubType()) - parser.Z80_FLAG_NZ
+		b := byte(0xc0 | flag<<3)
+		return &object.Code{Line: node.LineNumber(), Code: []byte{b}}
+	}
+	e.logger.Error(
+		fmt.Sprintf(logger.E017, node.Op1.String()), node.LineNumber())
+	return object.ERROR
+}
+
+func (e *Evaluator) evalZ80Instruction2(node *parser.Z80Instruction, env *object.Environment) object.Object {
+	switch node.NodeSubType() {
+	case parser.Z80_INST_LD:
+		return e.evalZ80LD(node, env)
+	default:
+		e.logger.Error(fmt.Sprintf(logger.E999, node), node.LineNumber())
+		return object.ERROR
+	}
+}
+
+func (e *Evaluator) evalZ80LD(node *parser.Z80Instruction, env *object.Environment) object.Object {
+	switch node.Op1.NodeType() {
+	case parser.Z80_REG8:
+		r1 := int(node.Op1.NodeSubType())
+		if r1 > parser.Z80_REG_A {
+			return object.NULL
+		}
+		switch node.Op2.NodeType() {
+		case parser.Z80_REG8:
+			// LD r,r'
+			r2 := int(node.Op2.NodeSubType())
+			if r2 > parser.Z80_REG_A {
+				return object.NULL
+			}
+			b := 0xc0
+			b |= ((r1 - parser.Z80_REG_B) << 3) | (r2 - parser.Z80_REG_B)
+			return &object.Code{Line: node.LineNumber(), Code: []byte{byte(b)}}
+		default:
+			op2 := e.Eval(node.Op2, env)
+			if op2.Type() == object.NUMBER_OBJ {
+				// LD r,n
+				v := op2.(*object.NumberObject).Value
+				bv, ok := e.intToByte(v)
+				if !ok {
+					e.logger.Warning(fmt.Sprintf(logger.W001, v, v), node.LineNumber())
+					bv = byte(v & 0xff)
+				}
+				b := 0x06
+				b |= (r1 - parser.Z80_REG_B) << 3
+				return &object.Code{Line: node.LineNumber(), Code: []byte{byte(b), bv}}
+			} else if op2.Type() == object.NULL_OBJ {
+				e.logger.Error(fmt.Sprintf("error expr: %s", node.Op2.String()), 0)
+			}
+		}
+	default:
+		return object.NULL
+	}
+	return object.NULL
+}
+
+func (e *Evaluator) intToByte(n int) (byte, bool) {
+	switch {
+	case 0 <= n && n <= 255:
+		return byte(n), true
+	case -128 <= n && n < 0:
+		return byte(n & 0xff), true
+	default:
+		return 0, false
+	}
+}
--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -372,21 +372,21 @@ func (e *Evaluator) evalNumberInfixExpression(opCode int, op1, op2 object.Object
 	case '^':
 		return &object.NumberObject{Value: v1 ^ v2}
 	case parser.EQ:
-		return &object.NumberObject{Value: e.toBool(v1 == v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 == v2)}
 	case parser.NEQ:
-		return &object.NumberObject{Value: e.toBool(v1 != v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 != v2)}
 	case '<':
-		return &object.NumberObject{Value: e.toBool(v1 < v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 < v2)}
 	case parser.LE:
-		return &object.NumberObject{Value: e.toBool(v1 <= v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 <= v2)}
 	case '>':
-		return &object.NumberObject{Value: e.toBool(v1 > v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 > v2)}
 	case parser.GE:
-		return &object.NumberObject{Value: e.toBool(v1 >= v2)}
+		return &object.NumberObject{Value: e.boolToInt(v1 >= v2)}
 	case parser.OR:
-		return &object.NumberObject{Value: e.toBool(v1 != 0 || v2 != 0)}
+		return &object.NumberObject{Value: e.boolToInt(v1 != 0 || v2 != 0)}
 	case parser.AND:
-		return &object.NumberObject{Value: e.toBool(v1 != 1 && v2 != 1)}
+		return &object.NumberObject{Value: e.boolToInt(v1 != 1 && v2 != 1)}
 	default:
 		e.logger.Error(fmt.Sprintf(logger.E016, string(rune(opCode))), lineNumber)
 		return object.ERROR
@@ -423,108 +423,6 @@ func (e *Evaluator) evalPrefixExpression(opCode int, op object.Object, lineNumbe
 
 func (e *Evaluator) boolToInt(value bool) int {
 	if value {
-		return 1
-	} else {
-		return 0
-	}
-}
-
-func (e *Evaluator) evalZ80Instruction(node *parser.Z80Instruction, env *object.Environment) object.Object {
-	switch node.NodeType() {
-	case parser.Z80_INST0:
-		info := Z80CodeTable0[int(node.OpCode)]
-		obj := &object.Code{Line: node.LineNumber(), Code: make([]byte, len(info.Bytes))}
-		copy(obj.Code, info.Bytes)
-		return obj
-	case parser.Z80_INST1:
-		if node.OpCode == parser.Z80_INST_RET {
-			return e.generateRET(node, env)
-		}
-		return object.NULL
-	case parser.Z80_INST2:
-		return e.evalZ80Instruction2(node, env)
-	default:
-		return object.NULL
-	}
-}
-
-func (e *Evaluator) generateRET(node *parser.Z80Instruction, env *object.Environment) object.Object {
-	if node.Op1 == nil {
-		return &object.Code{Line: node.LineNumber(), Code: []byte{0xc9}}
-	}
-	if node.Op1.NodeType() == parser.Z80_FLAG {
-		flag := int(node.Op1.NodeSubType()) - parser.Z80_FLAG_NZ
-		b := byte(0xc0 | flag<<3)
-		return &object.Code{Line: node.LineNumber(), Code: []byte{b}}
-	}
-	e.logger.Error(
-		fmt.Sprintf(logger.E017, node.Op1.String()), node.LineNumber())
-	return object.ERROR
-}
-
-func (e *Evaluator) evalZ80Instruction2(node *parser.Z80Instruction, env *object.Environment) object.Object {
-	switch node.NodeSubType() {
-	case parser.Z80_INST_LD:
-		return e.evalZ80LD(node, env)
-	default:
-		e.logger.Error(fmt.Sprintf(logger.E999, node), node.LineNumber())
-		return object.ERROR
-	}
-}
-
-func (e *Evaluator) evalZ80LD(node *parser.Z80Instruction, env *object.Environment) object.Object {
-	switch node.Op1.NodeType() {
-	case parser.Z80_REG8:
-		r1 := int(node.Op1.NodeSubType())
-		if r1 > parser.Z80_REG_A {
-			return object.NULL
-		}
-		switch node.Op2.NodeType() {
-		case parser.Z80_REG8:
-			// LD r,r'
-			r2 := int(node.Op2.NodeSubType())
-			if r2 > parser.Z80_REG_A {
-				return object.NULL
-			}
-			b := 0xc0
-			b |= ((r1 - parser.Z80_REG_B) << 3) | (r2 - parser.Z80_REG_B)
-			return &object.Code{Line: node.LineNumber(), Code: []byte{byte(b)}}
-		default:
-			op2 := e.Eval(node.Op2, env)
-			if op2.Type() == object.NUMBER_OBJ {
-				// LD r,n
-				v := op2.(*object.NumberObject).Value
-				bv, ok := e.toByte(v)
-				if !ok {
-					e.logger.Warning(fmt.Sprintf(logger.W001, v, v), node.LineNumber())
-					bv = byte(v & 0xff)
-				}
-				b := 0x06
-				b |= (r1 - parser.Z80_REG_B) << 3
-				return &object.Code{Line: node.LineNumber(), Code: []byte{byte(b), bv}}
-			} else if op2.Type() == object.NULL_OBJ {
-				e.logger.Error(fmt.Sprintf("error expr: %s", node.Op2.String()), 0)
-			}
-		}
-	default:
-		return object.NULL
-	}
-	return object.NULL
-}
-
-func (e *Evaluator) toByte(n int) (byte, bool) {
-	switch {
-	case 0 <= n && n <= 255:
-		return byte(n), true
-	case -128 <= n && n < 0:
-		return byte(n & 0xff), true
-	default:
-		return 0, false
-	}
-}
-
-func (e *Evaluator) toBool(b bool) int {
-	if b {
 		return 1
 	} else {
 		return 0
