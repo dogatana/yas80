63083a2a06d8478fb1d83867194cbf2db636bda4
Toshihiko Ichida <dogatana@gmail.com>
2025-11-29 06:20:18 +0900

    Lexer lineNumber/filename/curChar を Lexer.ctx へ移動

--- parser/lexer.go
+++ parser/lexer.go
@@ -26,11 +26,7 @@ type Lexer struct {
 	scanner *bufio.Scanner
 	isEOF   bool
 	text    []byte
-	//	index      int
-	curChar    rune
-	lineNumber int
-	filename   string
-	//
+
 	logger   *logger.Logger
 	program  *Program
 	callback FileBlockProvider
@@ -47,20 +43,20 @@ func NewLexer(filename string, fb *fileblock.FileBlock, logger *logger.Logger) *
 
 // yyLexer インターフェースメソッド
 func (l *Lexer) Lex(lval *yySymType) int {
-	if l.ctx == nil {
-		l.ctx = &LexerContext{
-			fileBlock:  l.callback(),
-			index:      0,
-			curChar:    0,
-			lineNumber: 0,
-		}
-	}
-	if l.ctx.fileBlock == nil {
-		// これ以上 FileBlock が得られない場合 EOF
-		tok := Token{TokenType: 0, Literal: "[EOF]", LineNumber: l.lineNumber}
-		lval.token = tok
-		return int(tok.TokenSubType)
-	}
+	// if l.ctx == nil {
+	// 	l.ctx = &LexerContext{
+	// 		fileBlock:  l.callback(),
+	// 		index:      0,
+	// 		curChar:    0,
+	// 		lineNumber: 0,
+	// 	}
+	// }
+	// if l.ctx.fileBlock == nil {
+	// 	// これ以上 FileBlock が得られない場合 EOF
+	// 	tok := Token{TokenType: 0, Literal: "[EOF]", LineNumber: l.ctx.lineNumber}
+	// 	lval.token = tok
+	// 	return int(tok.TokenSubType)
+	// }
 	tok := l.NextToken()
 	lval.token = tok
 	return int(tok.TokenType)
@@ -73,7 +69,7 @@ func (l *Lexer) Error(msg string, args ...any) {
 
 	switch len(args) {
 	case 0: // Error(msg)
-		line = l.lineNumber
+		line = l.ctx.lineNumber
 	case 1: // Error(msg, lineNumber)
 		n, ok := args[0].(int)
 		if !ok {
@@ -81,7 +77,7 @@ func (l *Lexer) Error(msg string, args ...any) {
 		}
 		line = n
 	case 2:
-		line = l.lineNumber
+		line = l.ctx.lineNumber
 		yyVAL = args[0].(*Token)
 		yylval = args[1].(*Token)
 		msg = modifyYaccError(msg, yyVAL, yylval)
@@ -115,87 +111,87 @@ func (l *Lexer) NextToken() Token {
 LINE_CONT:
 	// 空白をスキップ
 	l.skipWhitespace()
-	// fmt.Printf("curChar %q, peekChar %q\n", string(l.curChar), string(l.peekChar()))
+	// fmt.Printf("curChar %q, peekChar %q\n", string(l.ctx.curChar), string(l.peekChar()))
 
 	// var tokType int
 	switch {
-	case l.curChar == EOF:
+	case l.ctx.curChar == EOF:
 		// EOF
-		return Token{TokenType: 0, Literal: "[EOF]", LineNumber: l.lineNumber}
-	case l.curChar == ';':
+		return Token{TokenType: 0, Literal: "[EOF]", LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == ';':
 		// コメント
-		for l.curChar != '\n' && l.curChar != EOF {
+		for l.ctx.curChar != '\n' && l.ctx.curChar != EOF {
 			l.nextChar()
 		}
 		l.nextChar()
-		return Token{TokenType: EOL, Literal: "\\n", LineNumber: l.lineNumber}
-	case l.curChar == '\\' && l.peekChar() == '\n':
+		return Token{TokenType: EOL, Literal: "\\n", LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '\\' && l.peekChar() == '\n':
 		// 行継続
 		l.nextChar()
 		l.nextChar()
 		goto LINE_CONT
-	case l.curChar == '\\':
+	case l.ctx.curChar == '\\':
 		// マルチステートメント
-		tok := Token{TokenType: EOL, Literal: "\\", LineNumber: l.lineNumber}
+		tok := Token{TokenType: EOL, Literal: "\\", LineNumber: l.ctx.lineNumber}
 		l.nextChar()
 		return tok
-	case l.curChar == '\n':
+	case l.ctx.curChar == '\n':
 		// EOL
 		l.nextChar()
-		return Token{TokenType: EOL, Literal: "\\n", LineNumber: l.lineNumber}
-	case l.curChar == '"':
+		return Token{TokenType: EOL, Literal: "\\n", LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '"':
 		// 文字列リテラル
 		s := l.readString()
 		l.nextChar()
 		l.nextChar()
-		return Token{TokenType: STRING, Literal: s, LineNumber: l.lineNumber}
-	case l.curChar == '+' || l.curChar == '-' || l.curChar == '^': // ADDSUB
-		ch := l.curChar
+		return Token{TokenType: STRING, Literal: s, LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '+' || l.ctx.curChar == '-' || l.ctx.curChar == '^': // ADDSUB
+		ch := l.ctx.curChar
 		l.nextChar()
-		return Token{TokenType: ADDSUB, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.lineNumber}
-	case l.curChar == '*' || l.curChar == '/': // MULDIV
-		ch := l.curChar
+		return Token{TokenType: ADDSUB, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '*' || l.ctx.curChar == '/': // MULDIV
+		ch := l.ctx.curChar
 		l.nextChar()
-		return Token{TokenType: MULDIV, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.lineNumber}
-	case l.isTowCharTokenStart(l.curChar):
-		tok := l.checkTwoCharToken(l.curChar)
-		tok.LineNumber = l.lineNumber
+		return Token{TokenType: MULDIV, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.ctx.lineNumber}
+	case l.isTowCharTokenStart(l.ctx.curChar):
+		tok := l.checkTwoCharToken(l.ctx.curChar)
+		tok.LineNumber = l.ctx.lineNumber
 		return tok
-	case l.isOneCharToken(l.curChar):
+	case l.isOneCharToken(l.ctx.curChar):
 		// 1文字トークン
-		ch = l.curChar
+		ch = l.ctx.curChar
 		l.nextChar()
-		return Token{TokenType: TokenType(ch), Literal: string(ch), LineNumber: l.lineNumber}
-	case l.curChar == '~':
-		ch = l.curChar
+		return Token{TokenType: TokenType(ch), Literal: string(ch), LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '~':
+		ch = l.ctx.curChar
 		l.nextChar()
-		return Token{TokenType: UNARY, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.lineNumber}
-	case l.curChar == '0' && (l.peekChar() == 'x' || l.peekChar() == 'X'):
+		return Token{TokenType: UNARY, TokenSubType: TokenSubType(ch), Literal: string(ch), LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '0' && (l.peekChar() == 'x' || l.peekChar() == 'X'):
 		// 16進数リテラル(0x)
 		l.nextChar() // '0'をスキップ
-		literal = "0" + string(l.curChar)
+		literal = "0" + string(l.ctx.curChar)
 		l.nextChar() // 'x'または'X'をスキップ
 		literal += l.readWord()
 		l.nextChar()
-		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.lineNumber}
-	case l.curChar == '$' && !l.isXDigit(l.peekChar()):
+		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '$' && !l.isXDigit(l.peekChar()):
 		// $ ローケーションカウンタ
-		tok := Token{TokenType: IDENT, Literal: "$", LineNumber: l.lineNumber}
+		tok := Token{TokenType: IDENT, Literal: "$", LineNumber: l.ctx.lineNumber}
 		l.nextChar()
 		return tok
-	case l.curChar == '$' || l.curChar == '%':
+	case l.ctx.curChar == '$' || l.ctx.curChar == '%':
 		// 16進数リテラル($) or 2進数リテラル
-		literal = string(l.curChar)
+		literal = string(l.ctx.curChar)
 		l.nextChar() // '$'をスキップ
 		literal += l.readWord()
 		l.nextChar()
-		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.lineNumber}
-	case l.isDigit(l.curChar):
+		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.ctx.lineNumber}
+	case l.isDigit(l.ctx.curChar):
 		// 10進、16進(0x)、2進(0b)、8進（0o)リテラル
 		literal = l.readWord()
 		l.nextChar()
-		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.lineNumber}
-	case l.isAlpha(l.curChar):
+		return Token{TokenType: NUMBER, Literal: literal, LineNumber: l.ctx.lineNumber}
+	case l.isAlpha(l.ctx.curChar):
 		// IDENT、予約語
 		literal = l.readWord()
 		if l.peekChar() == '.' {
@@ -203,44 +199,44 @@ LINE_CONT:
 			l.nextChar()
 			literal += l.readWord()
 			l.nextChar()
-			return Token{TokenType: DOT_IDENT, Literal: literal, LineNumber: l.lineNumber}
+			return Token{TokenType: DOT_IDENT, Literal: literal, LineNumber: l.ctx.lineNumber}
 		}
 		l.nextChar()
 		// AF’の対処
-		if len(literal) == 2 && l.curChar == '\'' {
+		if len(literal) == 2 && l.ctx.curChar == '\'' {
 			literal += "'"
 			l.nextChar()
 		}
 		// z80 予約語
 		tok, ok := z80ReservedWords[strings.ToUpper(literal)]
 		if ok {
-			tok.LineNumber = l.lineNumber
+			tok.LineNumber = l.ctx.lineNumber
 			return tok
 		}
 		// yas80 予約語
 		tok, ok = reservedWords[strings.ToUpper(literal)]
 		if ok {
-			tok.LineNumber = l.lineNumber
+			tok.LineNumber = l.ctx.lineNumber
 			return tok
 		}
 		// これ以外は識別子
-		return Token{TokenType: IDENT, Literal: literal, LineNumber: l.lineNumber}
-	case l.curChar == '@' || l.curChar == '.':
-		prefix := l.curChar
-		literal = string(l.curChar)
+		return Token{TokenType: IDENT, Literal: literal, LineNumber: l.ctx.lineNumber}
+	case l.ctx.curChar == '@' || l.ctx.curChar == '.':
+		prefix := l.ctx.curChar
+		literal = string(l.ctx.curChar)
 		l.nextChar()
 		literal += l.readWord()
 		l.nextChar()
 		if prefix == '@' {
-			return Token{TokenType: AT_IDENT, Literal: literal, LineNumber: l.lineNumber}
+			return Token{TokenType: AT_IDENT, Literal: literal, LineNumber: l.ctx.lineNumber}
 		} else {
-			return Token{TokenType: LOCAL_IDENT, Literal: literal, LineNumber: l.lineNumber}
+			return Token{TokenType: LOCAL_IDENT, Literal: literal, LineNumber: l.ctx.lineNumber}
 		}
 
 	default:
-		literal = string(l.curChar)
+		literal = string(l.ctx.curChar)
 		l.nextChar()
-		return Token{TokenType: INVALID, Literal: literal, LineNumber: l.lineNumber}
+		return Token{TokenType: INVALID, Literal: literal, LineNumber: l.ctx.lineNumber}
 	}
 }
 
@@ -258,32 +254,32 @@ func (l *Lexer) checkTwoCharToken(ch1 rune) Token {
 	switch {
 	// COMP
 	case ch1 == '<' && ch2 == '=':
-		tok = Token{TokenType: COMP, TokenSubType: LE, Literal: "<=", LineNumber: l.lineNumber}
+		tok = Token{TokenType: COMP, TokenSubType: LE, Literal: "<=", LineNumber: l.ctx.lineNumber}
 	case ch1 == '<' && ch2 == '<':
-		tok = Token{TokenType: SHIFT, TokenSubType: SL, Literal: "<<", LineNumber: l.lineNumber}
+		tok = Token{TokenType: SHIFT, TokenSubType: SL, Literal: "<<", LineNumber: l.ctx.lineNumber}
 	case ch1 == '>' && ch2 == '=':
-		tok = Token{TokenType: COMP, TokenSubType: GE, Literal: ">=", LineNumber: l.lineNumber}
+		tok = Token{TokenType: COMP, TokenSubType: GE, Literal: ">=", LineNumber: l.ctx.lineNumber}
 	case ch1 == '>' && ch2 == '>':
-		tok = Token{TokenType: SHIFT, TokenSubType: SR, Literal: ">>", LineNumber: l.lineNumber}
+		tok = Token{TokenType: SHIFT, TokenSubType: SR, Literal: ">>", LineNumber: l.ctx.lineNumber}
 	case ch1 == '<' || ch1 == '>':
-		tok = Token{TokenType: COMP, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.lineNumber}
+		tok = Token{TokenType: COMP, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.ctx.lineNumber}
 	case ch1 == '=' && ch2 == '=':
-		tok = Token{TokenType: COMP, TokenSubType: EQ, Literal: "==", LineNumber: l.lineNumber}
+		tok = Token{TokenType: COMP, TokenSubType: EQ, Literal: "==", LineNumber: l.ctx.lineNumber}
 	case ch1 == '!' && ch2 == '=':
-		tok = Token{TokenType: COMP, TokenSubType: NEQ, Literal: "!=", LineNumber: l.lineNumber}
+		tok = Token{TokenType: COMP, TokenSubType: NEQ, Literal: "!=", LineNumber: l.ctx.lineNumber}
 	case ch1 == '!':
-		return Token{TokenType: UNARY, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.lineNumber}
+		return Token{TokenType: UNARY, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.ctx.lineNumber}
 	case ch1 == '&' && ch2 == '&':
-		tok = Token{TokenType: AND, TokenSubType: 0, Literal: "&&", LineNumber: l.lineNumber}
+		tok = Token{TokenType: AND, TokenSubType: 0, Literal: "&&", LineNumber: l.ctx.lineNumber}
 	case ch1 == '&':
-		return Token{TokenType: MULDIV, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.lineNumber}
+		return Token{TokenType: MULDIV, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.ctx.lineNumber}
 	case ch1 == '|' && ch2 == '|':
-		tok = Token{TokenType: OR, TokenSubType: 0, Literal: "||", LineNumber: l.lineNumber}
+		tok = Token{TokenType: OR, TokenSubType: 0, Literal: "||", LineNumber: l.ctx.lineNumber}
 	case ch1 == '|':
-		return Token{TokenType: ADDSUB, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.lineNumber}
+		return Token{TokenType: ADDSUB, TokenSubType: TokenSubType(ch1), Literal: string(ch1), LineNumber: l.ctx.lineNumber}
 	default:
 		// 1文字トークンを返す
-		return Token{TokenType: TokenType(ch1), Literal: string(rune(ch1)), LineNumber: l.lineNumber}
+		return Token{TokenType: TokenType(ch1), Literal: string(rune(ch1)), LineNumber: l.ctx.lineNumber}
 	}
 	l.nextChar()
 	return tok
@@ -291,31 +287,31 @@ func (l *Lexer) checkTwoCharToken(ch1 rune) Token {
 
 func (l *Lexer) nextChar() {
 	if l.isEOF {
-		l.curChar = EOF
+		l.ctx.curChar = EOF
 		return
 	}
 	if l.ctx.index == 0 {
 		scanned := l.scanner.Scan()
 		if !scanned {
-			if l.curChar != '\n' {
-				l.curChar = '\n'
+			if l.ctx.curChar != '\n' {
+				l.ctx.curChar = '\n'
 				return
 			}
-			l.curChar = EOF
+			l.ctx.curChar = EOF
 			l.isEOF = true
 			return
 		}
-		l.lineNumber++
+		l.ctx.lineNumber++
 		l.text = []byte(l.scanner.Text())
 	}
 	if l.ctx.index >= len(l.text) {
-		l.curChar = '\n'
+		l.ctx.curChar = '\n'
 		l.ctx.index = 0
 		return
 	}
 	start := l.ctx.index
 	l.ctx.index += l.charSize(l.text[l.ctx.index])
-	l.curChar = []rune(string(l.text[start:l.ctx.index]))[0]
+	l.ctx.curChar = []rune(string(l.text[start:l.ctx.index]))[0]
 }
 
 func (l *Lexer) peekChar() rune {
@@ -326,7 +322,7 @@ func (l *Lexer) peekChar() rune {
 }
 
 func (l *Lexer) skipWhitespace() {
-	for !l.isEOF && (l.curChar == ' ' || l.curChar == '\t' || l.curChar == '\r') {
+	for !l.isEOF && (l.ctx.curChar == ' ' || l.ctx.curChar == '\t' || l.ctx.curChar == '\r') {
 		l.nextChar()
 	}
 }
