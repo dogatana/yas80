bc5a0bd3066b1d4e909533279ca2333fc692ea81
Toshihiko Ichida <dogatana@gmail.com>
2025-12-12 21:12:09 +0900

    SymbolObject.String() を改善

--- object/symbol.go
+++ object/symbol.go
@@ -17,10 +17,10 @@ const (
 )
 
 var symbolTypeNames map[SymbolType]string = map[SymbolType]string{
-	SYM_UNKNOWN: "Unknown",
-	SYM_CONST:   "Const",
-	SYM_LABEL:   "Label",
-	SYM_VAR:     "Var",
+	SYM_UNKNOWN: "SYM_UNKNOWN",
+	SYM_CONST:   "SYM_CONST",
+	SYM_LABEL:   "SYM_LABEL",
+	SYM_VAR:     "SYM_VAR",
 }
 
 // symbol
@@ -35,8 +35,8 @@ type SymbolObject struct {
 
 func (s *SymbolObject) Type() ObjectType { return SYMBOL_OBJ }
 func (s *SymbolObject) String() string {
-	str := fmt.Sprintf("Symbol{Name:%q, SymType: %s, Value: %T",
-		s.Name, symbolTypeNames[s.SymType], s.Value)
+	str := fmt.Sprintf("Symbol{%q, %s, %s",
+		s.Name, symbolTypeNames[s.SymType], s.Value.String())
 	if len(s.DependsOn) > 0 {
 		str += ", [" + strings.Join(s.DependsOn, ",") + "]"
 	}
