2829a6fed31a345613dcfea7801edebdd0e1dbbb
Toshihiko Ichida <dogatana@gmail.com>
2025-12-20 06:50:47 +0900

    エラーテストコードを errtest フォルダへ移動

--- errtest/errtest_test.go
+++ errtest/errtest_test.go
@@ -1,4 +1,4 @@
-package evaluator
+package errtest
 
 import (
 	"testing"
---
+++ errtest/testhelper_test.go
@@ -0,0 +1,73 @@
+package errtest
+
+import (
+	"bytes"
+	"regexp"
+	"strings"
+	"yas80/evaluator"
+	"yas80/fileblock"
+	"yas80/logger"
+	"yas80/object"
+	"yas80/parser"
+)
+
+func evaluateErrorInput(input string, logger *logger.Logger, env object.Environment) {
+	prog := parseErrorTextForTest(input, logger)
+	if len(logger.Errors) > 0 {
+		return
+	}
+
+	e := evaluator.New(logger)
+	e.Resolved = true
+	var obj object.Object
+	var i int
+	for i = 0; i < 256; i++ {
+		e.Resolved = true
+		obj = e.EvalProgram(prog, env)
+		e.EvalEnv(env)
+		e.CheckSymbols(env)
+		if len(logger.Errors) > 0 {
+			return
+		}
+		if e.Resolved {
+			break
+		}
+	}
+	// finalize
+	code := evaluator.CollectCode(obj.(*object.ProgramObject))
+	codeStable := false
+	for i = 0; i < 256 && !codeStable; i++ {
+		obj = e.EvalProgram(prog, env)
+		newCode := evaluator.CollectCode(obj.(*object.ProgramObject))
+		codeStable = bytes.Equal(code, newCode)
+		if !codeStable {
+			code = newCode
+		}
+	}
+}
+
+func parseErrorTextForTest(input string, logger *logger.Logger) *parser.Program {
+	file := "<string>"
+	fb := fileblock.New(file, []byte(input))
+	l := parser.NewLexer(fb, logger)
+	return parser.Parse(l)
+}
+
+func hasError(logger *logger.Logger, expected string) bool {
+	re := regexp.MustCompile(`\.?%.\.?`)
+	ss := re.Split(expected, -1)
+
+	for _, emsg := range logger.Errors {
+		result := true
+		for _, s := range ss {
+			if !strings.Contains(emsg.Message, s) {
+				result = false
+				break
+			}
+		}
+		if result {
+			return result
+		}
+	}
+	return false
+}
