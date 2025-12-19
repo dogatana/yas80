8e7d61491ee04a30694c3e11fb3cf541c7b0b32f
Toshihiko Ichida <dogatana@gmail.com>
2025-12-19 10:00:04 +0900

    TestExitm テスト追加

--- evaluator/eval_macro_test.go
+++ evaluator/eval_macro_test.go
@@ -26,3 +26,48 @@ func TestMacro(t *testing.T) {
 		}
 	}
 }
+
+func TestExitm(t *testing.T) {
+	tests := []struct {
+		input string
+		code  []byte
+	}{
+		{`
+			test1 macro
+			ld a, 0
+			exitm
+			ld a, 1
+			endm
+
+			test2 macro
+			ld hl, 0
+			test1
+			ld hl, 1
+			endm
+
+			test1
+			test2
+		`, []byte{
+			0x3E, 0x00, // ld a, 0
+			0x21, 0x00, 0x00, // ld hl, 0
+			0x3E, 0x00, // ld a, 0
+			0x21, 0x01, 0x00, // ld hl, 1
+		},
+		},
+	}
+
+	for tn, tt := range tests {
+		env := object.NewEnvironment(nil)
+		logger := logger.New("<eval test>")
+		input := tt.input
+		expected := tt.code
+
+		prog := evaluateInput(t, input, logger, env)
+		logger.Print()
+		result := CollectCode(prog)
+
+		if !bytesEqual(result, expected) {
+			t.Errorf("[%d] expected %d bytes. got %d bytes", tn, len(expected), len(result))
+		}
+	}
+}
