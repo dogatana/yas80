bf623282a41743ba9837b3b2a21a683793e0715a
Toshihiko Ichida <dogatana@gmail.com>
2025-12-19 13:54:39 +0900

    TestMacroIf テストケース追加

--- evaluator/eval_macro_test.go
+++ evaluator/eval_macro_test.go
@@ -54,6 +54,29 @@ func TestExitm(t *testing.T) {
 			0x21, 0x01, 0x00, // ld hl, 1
 		},
 		},
+	}
+
+	for tn, tt := range tests {
+		env := object.NewEnvironment(nil)
+		logger := logger.New("<eval test>")
+		input := tt.input
+		expected := tt.code
+
+		prog := evaluateInput(t, input, logger, env)
+		logger.Print()
+		result := CollectCode(prog)
+
+		if !bytesEqual(result, expected) {
+			t.Errorf("[%d] expected %d bytes. got %d bytes", tn, len(expected), len(result))
+		}
+	}
+}
+
+func TestMacroIf(t *testing.T) {
+	tests := []struct {
+		input string
+		code  []byte
+	}{
 		{
 			`test macro arg \ if arg > 0 \ ld a, 1 \ else \ ld a, $ff \ endif \ endm \ test 1`,
 			[]byte{0x3E, 0x01},
@@ -61,6 +84,18 @@ func TestExitm(t *testing.T) {
 		{
 			`test macro arg \ if arg > 0 \ ld a, 1 \ else \ ld a, $ff \ endif \ endm \ test -1`,
 			[]byte{0x3E, 0xFF},
+		},
+		{
+			`test macro arg \ ld a, 0 \ exitm if arg == 0 \ ld a, 2 \ exitm if arg == 1 \ ld a, $ff \endm \ test 0`,
+			[]byte{0x3E, 0x00},
+		},
+		{
+			`test macro arg \ ld a, 0 \ exitm if arg == 0 \ ld a, 1 \ exitm if arg == 1 \ ld a, $ff \endm \ test 1`,
+			[]byte{0x3E, 0x00, 0x3E, 0x01},
+		},
+		{
+			`test macro arg \ ld a, 0 \ exitm if arg == 0 \ ld a, 1 \ exitm if arg == 1 \ ld a, $ff \endm \ test 2`,
+			[]byte{0x3E, 0x00, 0x3E, 0x01, 0x3E, 0xFF},
 		},
 	}
 
