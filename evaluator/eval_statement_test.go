ba5a73fb13be796b9aa3df8bf0863d95fe24b45c
Toshihiko Ichida <dogatana@gmail.com>
2025-12-27 20:46:43 +0900

    TestConstStatement テストケース追加

--- evaluator/eval_statement_test.go
+++ evaluator/eval_statement_test.go
@@ -61,25 +61,40 @@ func TestConstStatement(t *testing.T) {
 	tests := []struct {
 		input string
 		syms  []SymValue
+		err   string
 	}{
-		{"const abc ## 123 = 456",
-			[]SymValue{{"ABC123", 456}}},
-		{"const abc ## (100 + 23) = 456",
-			[]SymValue{{"ABC123", 456}}},
-		{"const abc ## (100 + 23) = def ## 456 \\ const def ## (400 + 56) = 999",
-			[]SymValue{{"ABC123", 999}}},
-		{"abc ## 123 equ 456",
-			[]SymValue{{"ABC123", 456}}},
-		{"abc ## (100 + 23) equ 456",
-			[]SymValue{{"ABC123", 456}}},
-		{"abc ## (100 + 23) equ def ## 456 \\ const def ## (400 + 56) = 999",
-			[]SymValue{{"ABC123", 999}}},
+		// 0-
+		{input: `const abc = 123`, syms: []SymValue{{"ABC", 123}}},
+		{input: `const abc = 1 + 2 * 3`, syms: []SymValue{{"ABC", 7}}},
+		{input: `const abc = xyz + 1 \ const xyz = 9`, syms: []SymValue{{"ABC", 10}, {"XYZ", 9}}},
+		{input: `const abc = xyz`, err: errcode.ESYM_UNDEF},
+		{input: `const abc = xyz`, err: errcode.ESYM_NULL},
+
+		// 5-
+		{input: "const abc ## 123 = 456", syms: []SymValue{{"ABC123", 456}}},
+		{input: "const abc ## (100 + 23) = 456", syms: []SymValue{{"ABC123", 456}}},
+		{input: "const abc ## (100 + 23) = def ## 456 \\ const def ## (400 + 56) = 999", syms: []SymValue{{"ABC123", 999}}},
+		{input: `const abc ## "_XYZ" = 123`, syms: []SymValue{{"ABC_XYZ", 123}}},
+		{input: `const abc ## "_xyz" = 123`, syms: []SymValue{{"ABC_XYZ", 123}}},
+
+		// 10-
+		{input: "abc ## (100 + 23) equ def ## 456 \\ const def ## (400 + 56) = 999", syms: []SymValue{{"ABC123", 999}}},
+		{input: `const abc ## a = 1`, err: errcode.ESYM_CONCAT_TYPE},
+		{input: `const abc ## hl = 1`, err: errcode.ESYM_CONCAT_TYPE},
+		{input: `const abc ## cy = 1`, err: errcode.ESYM_CONCAT_TYPE},
+		{input: `const abc ## cy ## 1 = 1`, err: errcode.ESYNTAX}, // syntax error
 	}
 	for tn, tt := range tests {
 		env := object.NewEnvironment(nil)
 		logger := logging.New("<eval test>")
 		_, e := evalInput(tt.input, logger, env)
-		testEvalResult(t, tn, "", e)
+		testEvalResult(t, tn, tt.err, e)
+
+		// error, warning, information
+		if tt.err != "" {
+			testLogMessage(t, tn, tt.err, e.logger)
+			continue
+		}
 
 		getter := func(name string) (*object.SymbolObject, bool) {
 			return e.getSymbolFromEnv(name, env)
@@ -91,43 +106,20 @@ func TestConstStatement(t *testing.T) {
 func TestProcStatement(t *testing.T) {
 	tests := []struct {
 		input string
-		syms  []SymValue
-	}{
-		{`test proc \ const abc ## (100 + 23) = 456 \ endp`,
-			[]SymValue{{"ABC123", 456}}},
-		{`test proc \ const abc ## (100 + 23) = def ## 456 \ const def ## (400 + 56) = 999 \ endp`,
-			[]SymValue{{"ABC123", 999}}},
-		{`test proc \ abc ## 123 equ 456 \ endp`,
-			[]SymValue{{"ABC123", 456}}},
-		{`test proc \ abc ## (100 + 23) equ 456 \ endp`,
-			[]SymValue{{`ABC123`, 456}}},
-		{`test proc \ abc ## (100 + 23) equ def ## 456 \ const def ## (400 + 56) = 999  \ endp`,
-			[]SymValue{{"ABC123", 999}}},
-	}
-	for tn, tt := range tests {
-		if tt.input == "" {
-			continue
-		}
-		env := object.NewEnvironment(nil)
-		logger := logging.New("<eval test>")
-		_, e := evalInput(tt.input, logger, env)
-		testEvalResult(t, tn, "", e)
-
-		getter := func(name string) (*object.SymbolObject, bool) {
-			return e.getSymbolFromEnv(name, env)
-		}
-		testSymValues(t, tn, tt.syms, getter)
-	}
-}
-
-func TestProcLabel(t *testing.T) {
-	tests := []struct {
-		input string
 		code  []byte
 		syms  []SymValue
 		err   string
 	}{
-		// 0-
+		// 0- 単純ケース
+		{input: `abc proc \ nop \ .ret: ret \ nop \ const .xyz = 123 \ endp`,
+			code: []byte{0, 0xc9, 0},
+			syms: []SymValue{{"ABC.RET", 1}, {"ABC.XYZ", 123}},
+		},
+		{},
+		{},
+		{},
+		{},
+		// 5-
 		{`abc proc \ ld a, .abc \ .abc: nop \ endp \ xyz proc \ ld a, .abc \ .abc: ret \ endp`,
 			[]byte{0x3e, 0x02, 0x00, 0x3e, 0x05, 0xc9}, // ld a,2 \ nop \ ld a,5 \ ret
 			[]SymValue{
@@ -166,7 +158,15 @@ func TestProcLabel(t *testing.T) {
 			},
 			"",
 		},
-		// 5- error case
+
+		// 10- シンボル結合
+		{input: `test proc \ const abc ## (100 + 23) = 456 \ endp`, syms: []SymValue{{"ABC123", 456}}},
+		{input: `test proc \ const abc ## (100 + 23) = def ## 456 \ const def ## (400 + 56) = 999 \ endp`, syms: []SymValue{{"ABC123", 999}}},
+		{input: `test proc \ abc ## 123 equ 456 \ endp`, syms: []SymValue{{"ABC123", 456}}},
+		{input: `test proc \ abc ## (100 + 23) equ 456 \ endp`, syms: []SymValue{{`ABC123`, 456}}},
+		{input: `test proc \ abc ## (100 + 23) equ def ## 456 \ const def ## (400 + 56) = 999  \ endp`, syms: []SymValue{{"ABC123", 999}}},
+
+		// 15- error case
 		{input: `abc proc \ nop \ endp \ abc proc \ ret \ endp`, err: errcode.EPROC_DUP},
 		{input: `const abc = 1 \ abc proc \ ret \ endp`, err: errcode.EPROC_USED},
 		{input: `abc: nop \ abc proc \ ret \ endp`, err: errcode.EPROC_USED},
