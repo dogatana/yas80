cdf3511691b91bd219c5270a8a5ed10562f8cbb1
Toshihiko Ichida <dogatana@gmail.com>
2026-01-04 13:31:58 +0900

    Context.Offset, Context.Source フィールド追加

--- fileblock/context.go
+++ fileblock/context.go
@@ -4,14 +4,20 @@ import "fmt"
 
 type Context struct {
 	FileBlock *FileBlock
-	Line      int // 1-
-	Index     int // 0- FileBox.Contet の Index
+	Line      int      // 1-
+	Index     int      // 0- FileBox.Contet の Index
+	Offset    int      // 0 マクロ未展開, 1- マクロ展開後
+	Source    *Context // マクロ定義 Context
 }
 
 func (c *Context) String() string {
-	return fmt.Sprintf("%q:(%d,%d)", c.FileBlock.Filename, c.Line, c.Index)
+	ret := fmt.Sprintf("%q:%d,%d", c.FileBlock.Filename, c.Line, c.Index)
+	if c.Source != nil {
+		ret += fmt.Sprintf("[%q:%d,%d)]", c.FileBlock.Filename, c.Line, c.Index)
+	}
+	return ret
 }
 
-func (c *Context) Equal(other *Context) bool {
-	return c.FileBlock == other.FileBlock && c.Line == other.Line && c.Index == other.Index
+func (c *Context) Equal(o *Context) bool {
+	return c.FileBlock == o.FileBlock && c.Line == o.Line && c.Index == o.Index && c.Offset == o.Offset
 }
