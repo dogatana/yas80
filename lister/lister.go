e6a0cf9997e0a41d2dd2dfefe8cb1707d334962c
Toshihiko Ichida <dogatana@gmail.com>
2026-02-06 10:45:58 +0900

    CodeObject の Code サイズが 8 を超える複数行リスト出力対応

--- lister/lister.go
+++ lister/lister.go
@@ -29,12 +29,11 @@ func (l *Lister) ProgramList(out io.Writer) {
 		sline string
 		err   error
 	)
-	line := 1
+	lnum := 1
 
 	w := bufio.NewWriter(out)
 
 	for _, obj := range objs {
-		var code *object.CodeObject
 		if obj.Type() == object.OBJ_FILE {
 			file := obj.(*object.FileObject).Filename
 			// (1)ファイル名出力
@@ -42,48 +41,68 @@ func (l *Lister) ProgramList(out io.Writer) {
 			continue
 		}
 
-		code, ok := obj.(*object.CodeObject)
-		if !ok || len(code.Code) == 0 {
+		var co *object.CodeObject
+		co, ok := obj.(*object.CodeObject)
+		if !ok || len(co.Code) == 0 {
 			continue
 		}
-
-		ctx := code.Context
+		ctx := co.Context
 
 		if fc == nil {
 			fc = ctx.FileContent
 		}
 
-		for line < ctx.Line {
-			sline, err := fc.GetLine(line)
+		// CodeObject の行までソース行を進める
+		for lnum < ctx.Line {
+			sline, err = fc.GetLine(lnum)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-			fmt.Printf("%6d: %30s\t%s\n", line, "", sline)
-			line++
+			fmt.Printf("%6d  %30s\t%s\n", lnum, "", sline)
+			lnum++
 		}
-		sline, err = fc.GetLine(line)
+
+		sline, err = fc.GetLine(lnum)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(l.codeToLine(code) + sline)
-		line++
+
+		lines := l.codeToLines(co)
+		fmt.Println(lines[0] + sline)
+		for _, line := range lines[1:] {
+			fmt.Println(line)
+		}
+		lnum++
 	}
 	w.Flush()
-
-	for i := 148; i <= 151; i++ {
-		s, _ := fc.GetLine(i)
-		fmt.Printf("%d: %s\n", i, s)
-	}
 }
 
-func (l *Lister) codeToLine(code *object.CodeObject) string {
-	var buf bytes.Buffer
-	for _, b := range code.Code {
-		buf.WriteString(fmt.Sprintf("%02x", b))
+func (l *Lister) codeToLines(co *object.CodeObject) []string {
+	lines := []string{}
+
+	size := len(co.Code)
+	addr := co.Addr
+
+	for i := 0; i < size; i += 8 {
+		var j int
+		var buf bytes.Buffer
+		for j = 0; j < 8 && i+j < size; j++ {
+			buf.WriteString(fmt.Sprintf("%02x", co.Code[i+j]))
+		}
+		for j < 8 {
+			buf.WriteString("  ")
+			j++
+		}
+		if i == 0 {
+			lines = append(lines, fmt.Sprintf("%6d  %04x  %-16s  [%2d]\t", co.Context.Line, addr, buf.String(), co.CZ80))
+		} else {
+			lines = append(lines, fmt.Sprintf("        %04x  %-16s  [%2d]\t", addr, buf.String(), co.CZ80))
+		}
+		addr += 8
 	}
-	return fmt.Sprintf("%6d  %04x  %-16s  [%2d]\t", code.Context.Line, code.Addr, string(buf.String()), code.CZ80)
+	return lines
 }
 
 func printStatement(w io.Writer, stmt parser.Statement) {
