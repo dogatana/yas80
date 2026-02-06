ab6642c2eabda7eebd494b94eacd45e565c26a31
Toshihiko Ichida <dogatana@gmail.com>
2026-02-06 18:48:54 +0900

    util.Filter 追加

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,1 +1,12 @@
 package util
+
+// filter
+func Filter[T any](s []T, f func(T) bool) []T {
+	out := make([]T, 0, len(s))
+	for _, v := range s {
+		if f(v) {
+			out = append(out, v)
+		}
+	}
+	return out
+}
