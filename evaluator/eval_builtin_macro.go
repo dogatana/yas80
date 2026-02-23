9225838dff6edcd169daf99e781bf8733a2a561e
Toshihiko Ichida <dogatana@gmail.com>
2026-02-23 21:38:54 +0900

    組み込みマクロ ERROR/WARN/INFO 実装

--- evaluator/eval_builtin_macro.go
+++ evaluator/eval_builtin_macro.go
@@ -3,14 +3,22 @@ package evaluator
 import (
 	"fmt"
 	"yas80/errcode"
+	"yas80/logging"
 	"yas80/object"
 	"yas80/parser"
 )
 
+// 組み込みマクロの実行
 func (e *Evaluator) evalBuiltinMacro(stmt *parser.MacroCallStatement, env TEnv) (object.Object, bool) {
 	switch stmt.Name {
 	case "ALIGN":
 		return e.ebMacroAlign(stmt, env), true
+	case "ERROR":
+		return e.ebMacroLogMessage(logging.Err, stmt, env), true
+	case "WARN":
+		return e.ebMacroLogMessage(logging.Warn, stmt, env), true
+	case "INFO":
+		return e.ebMacroLogMessage(logging.Info, stmt, env), true
 	}
 	return object.NULL, false
 
@@ -90,4 +98,46 @@ func (e *Evaluator) ebMacroAlign(stmt *parser.MacroCallStatement, env TEnv) obje
 		e.logger.Error(err.Error(), stmt.Context)
 	}
 	return &object.CodeObject{Addr: addr, Code: code, Filled: true, Context: stmt.Context}
+}
+
+func (e *Evaluator) ebMacroLogMessage(msgType int, stmt *parser.MacroCallStatement, env TEnv) object.Object {
+
+	args := stmt.Args.Expressions
+
+	if len(args) != 1 {
+		e.logger.Error(fmt.Sprintf(errcode.EEBMAC_ARG_COUNT, stmt.Name), stmt.Context)
+		return object.ERROR
+	}
+
+	var str string
+	obj := e.evalExpression(args[0], env, stmt.Context)
+	switch obj := obj.(type) {
+	case *object.ErrorObject:
+		return obj
+	case *object.RefNotFoundObject:
+		return obj
+	case *object.NullObject:
+		e.logger.Error(fmt.Sprintf(errcode.EEBMAC_ARG_NULL, stmt.Name), stmt.Context)
+		return object.ERROR
+
+	case *object.NumberObject:
+		str = fmt.Sprintf("%d", obj.Value)
+
+	case *object.StringObject:
+		str = obj.Value
+
+	default:
+		e.logger.Error(fmt.Sprintf(errcode.EEBMAC_ARG_VALUE, stmt.Name), stmt.Context)
+		return object.ERROR
+	}
+
+	switch msgType {
+	case logging.Err:
+		e.logger.Error(str, stmt.Context)
+	case logging.Warn:
+		e.logger.Warning(str, stmt.Context)
+	case logging.Info:
+		e.logger.Info(str, stmt.Context)
+	}
+	return object.NULL // エラーではないので NULL を返す
 }
