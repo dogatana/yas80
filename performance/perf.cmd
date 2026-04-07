76e0b5efcca49c3053e87fd5fd46de660ac157e8
Toshihiko Ichida <dogatana@gmail.com>
2026-04-07 17:57:58 +0900

    性能計測 exe の参照方法変更

--- performance/perf.cmd
+++ performance/perf.cmd
@@ -2,19 +2,19 @@ rem min.asm
 py measure.py 100 "ailz80asm -bin -f -i min.asm"
 py measure.py 100 "java -jar tools80.jar -bin -tgt=z80  min.asm"
 py measure.py 100 "z80asm -b min.asm"
-py measure.py 100 "..\yas80 min.asm"
+py measure.py 100 "yas80 min.asm"
 py measure.py 100 "z80as min.asm"
 
 rem max.asm
 py measure.py 100 "ailz80asm -bin -f -i max.asm"
 py measure.py 100 "java -jar tools80.jar -bin -tgt=z80  max.asm"
 py measure.py 100 "z80asm -b max.asm"
-py measure.py 100 "..\yas80 max.asm"
+py measure.py 100 "yas80 max.asm"
 py measure.py 100 "z80as max.asm"
 
 rem label.asm
 py measure.py 100 "ailz80asm -bin -f -i label.asm"
 py measure.py 100 "java -jar tools80.jar -bin -tgt=z80  label.asm"
 py measure.py 100 "z80asm -b label.asm"
-py measure.py 100 "..\yas80 label.asm"
+py measure.py 100 "yas80 label.asm"
 py measure.py 100 "z80as label.asm"
